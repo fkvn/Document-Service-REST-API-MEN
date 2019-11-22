c0aeeb9f65595453ea65988ab9fa26449525b077
PHUC C  NGO <kevinngo@PHUCs-MacBook-Pro.local>
2019-11-22 08:02:53 -0800

    deleted duplicate hw4 folder

--- hw4/README.md
+++
@@ -1,2 +0,0 @@
-# Document-Service-REST-API-MEN
- MEN (MongoDB - Express - Nodejs) Application: Document Service REST API that supports the document operations. 
--- hw4/services/DatabaseServices.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  async getClient() {
-    const MongoClient = require('mongodb').MongoClient;
-    const url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?authSource=${process.env.DB_DATABASE}`
-    const client  = await MongoClient.connect(url, { useUnifiedTopology: true });
-    const db = client.db(`${process.env.DB_DATABASE}`)
-    return {client, db}
-  },
-}
